1283601d2e49853a666e697d4607e62206520355
Thibault Durand <tibo.durand@gmail.com>
2024-09-01 15:30:03 +0200

    WIP: bootstrap wordpress plugin

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '522ba0c07f24b24d3d0e');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'bc1ac08283cb80c83e43');
